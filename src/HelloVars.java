ff7f2e64505e4f3ec701fe2f2c0c1b335a21f53a
nowl.driesten <25991@ma-web.nl>
2018-09-27 10:05:01 +0200

    Initial commit

---
+++ src/HelloVars.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Hellovars {
+
+    public static void main(String[] args) {
+
+        String name = "Nowl";
+        String achternaam = "Driesten";
+        int leerjaar = 1;
+        double getal = 2.2;
+
+        System.out.println(name + " " + achternaam);
+
+        System.out.println("Hello Variables");
+
+        Scanner scan = new Scanner (System.in);
+        scan.next();
+        System.out.println(name );
+    }
+
+}
